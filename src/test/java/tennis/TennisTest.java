eab1014e2cbacdc0643616d69d5194ff964faf63
Omar MEBARKI <omar.mebarki@gmail.com>
2018-01-30 20:57:17 +0100

    Extract Players names to constants

--- src/test/java/tennis/TennisTest.java
+++ src/test/java/tennis/TennisTest.java
@@ -9,12 +9,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-import tennis.TennisGame;
-import tennis.TennisGame1;
 
 @RunWith(Parameterized.class)
 public class TennisTest {
 
+    public static final String PLAYER_1_NAME = "player1";
+    public static final String PLAYER_2_NAME = "player2";
     private int player1Score;
     private int player2Score;
     private String expectedScore;
@@ -73,16 +73,16 @@ public class TennisTest {
         int highestScore = Math.max(this.player1Score, this.player2Score);
         for (int i = 0; i < highestScore; i++) {
             if (i < this.player1Score)
-                game.wonPoint("player1");
+                game.wonPoint(PLAYER_1_NAME);
             if (i < this.player2Score)
-                game.wonPoint("player2");
+                game.wonPoint(PLAYER_2_NAME);
         }
         assertEquals(this.expectedScore, game.getScore());
     }
 
     @Test
     public void checkAllScoresTennisGame1() {
-        TennisGame1 game = new TennisGame1("player1", "player2");
+        TennisGame1 game = new TennisGame1(PLAYER_1_NAME, PLAYER_2_NAME);
         checkAllScores(game);
     }
 
