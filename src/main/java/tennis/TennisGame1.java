4577a390ff4dbd09d1f8366ee0c5a6c48f641cab
Omar MEBARKI <omar.mebarki@gmail.com>
2018-01-30 21:10:32 +0100

    Extract method computeScoreWhenPlayersHaveTheSame

--- src/main/java/tennis/TennisGame1.java
+++ src/main/java/tennis/TennisGame1.java
@@ -19,56 +19,56 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (this.playerScore1.getScore()==this.playerScore2.getScore())
-        {
-            switch (this.playerScore1.getScore())
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        }
-        else if (this.playerScore1.getScore()>=4 || this.playerScore2.getScore()>=4)
-        {
-            int minusResult = this.playerScore1.getScore()-this.playerScore2.getScore();
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = this.playerScore1.getScore();
-                else { score+="-"; tempScore = this.playerScore2.getScore();}
-                switch(tempScore)
-                {
+        int tempScore = 0;
+        if (this.playerScore1.getScore() == this.playerScore2.getScore()) {
+            score = computeScoreWhenPlayersHaveTheSame();
+        } else if (this.playerScore1.getScore() >= 4 || this.playerScore2.getScore() >= 4) {
+            int minusResult = this.playerScore1.getScore() - this.playerScore2.getScore();
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = this.playerScore1.getScore();
+                else {
+                    score += "-";
+                    tempScore = this.playerScore2.getScore();
+                }
+                switch (tempScore) {
                     case 0:
-                        score+="Love";
+                        score += "Love";
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score += "Fifteen";
                         break;
                     case 2:
-                        score+="Thirty";
+                        score += "Thirty";
                         break;
                     case 3:
-                        score+="Forty";
+                        score += "Forty";
                         break;
                 }
             }
+        }
+        return score;
+    }
+
+    private String computeScoreWhenPlayersHaveTheSame() {
+        String score;
+        switch (this.playerScore1.getScore()) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
         }
         return score;
     }
