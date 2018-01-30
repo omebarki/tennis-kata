2e96c3d2bfcbaa5d3b7342783aa4d7efd511ebb3
Omar MEBARKI <omar.mebarki@gmail.com>
2018-01-30 20:34:57 +0100

    Move classes from default package to tennis package

--- src/test/java/tennis/TennisTest.java
+++ src/test/java/tennis/TennisTest.java
@@ -1,13 +1,16 @@
+package tennis;
+
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import tennis.TennisGame;
+import tennis.TennisGame1;
 
 @RunWith(Parameterized.class)
 public class TennisTest {
